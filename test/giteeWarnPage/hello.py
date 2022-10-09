78eed2b4a36500d0d019bb2c66ae222430cce75e
Sourcery AI <>
2022-10-09 07:24:38 +0000

    'Refactored by Sourcery'

--- test/giteeWarnPage/hello.py
+++ test/giteeWarnPage/hello.py
@@ -32,7 +32,7 @@ window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    if event in [sg.WIN_CLOSED, 'Cancel']:  # if user closes window or clicks cancel
         break
     print('You entered ', values[0])
 
